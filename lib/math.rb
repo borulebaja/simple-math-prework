9009d059d686197cd22f8eda953017bf736c2a5c
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-24 19:26:23 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -22,6 +22,6 @@ def square_root(num)
   Math.sqrt(num)
 end
 
-def order_of_operation(num1, num2, num3, num4)
+
   #Hint:  __ + (( __ * __ ) / __ )
-end
+enddef order_of_operation(num1, num2, num3, num4)
